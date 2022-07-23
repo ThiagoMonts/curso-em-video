5f7c84e2a25f0ee09367fdee4efcfcf8ff6779c2
ThiagoMonts <thiagohonorato@hotmail.com>
2022-07-23 16:28:54 -0300

    Desafio 010

---
+++ Python/desafiosPython/desafio010/desafio010.py
@@ -0,0 +1,3 @@
+real = float(input('Quanto dinheiro você tem na carteira? R$ '))
+print(f'Com R$ {real:.2f} você consegue comprar US$ {real / 5.09:.2f}')
+print(f'Com R$ {real:.2f} você consegue comprar Euros {real / 5.35:.2f}')

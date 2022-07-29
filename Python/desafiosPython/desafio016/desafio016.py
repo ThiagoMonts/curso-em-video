599423f4295e25a14a1e9f12771ac7331827000d
ThiagoMonts <thiagohonorato@hotmail.com>
2022-07-29 11:10:44 -0300

    Desafio 016

---
+++ Python/desafiosPython/desafio016/desafio016.py
@@ -0,0 +1,8 @@
+'''
+//Solução com importação:
+from math import trunc
+numero = float(input('Digite um valor: '))
+print(f'O valor digitado foi {numero} e a sua porção inteira é {trunc(numero)}.')'''
+
+num = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num} e a sua porção inteira é {int(num)}.')

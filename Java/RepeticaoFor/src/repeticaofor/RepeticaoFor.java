5bcbf1cefed869583c870dedcc9e45e58c5cdd79
ThiagoMonts <thiagohonorato@hotmail.com>
2022-08-21 10:56:05 -0300

    Java - Exercícios

---
+++ Java/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author thiag
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=1; cc<=100; cc+=10){
+            System.out.println(cc);
+        }
+        
+        /* O comando acima pode ser representado da mesma maneira do comando abaixo
+        int cc=0;
+        while (cc<4){
+        System.out.println("Cambalhota");
+        cc++;
+        */
+    }
+    
+}

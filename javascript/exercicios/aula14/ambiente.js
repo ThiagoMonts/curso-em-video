ca7a0e7f0cecca4666f37c8acde3d0797cdc43b8
ThiagoMonts <thiagohonorato@hotmail.com>
2022-09-16 12:06:00 -0300

    JavaScript - Aula 14

---
+++ javascript/exercicios/aula14/ambiente.js
@@ -0,0 +1,30 @@
+/* 
+//Laço com while
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+//Laço com do while
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5) 
+*/
+
+//-------------------------
+/*
+//Laço for
+for(var c= 1; c <=5; c++) {
+    console.log(c)
+}
+*/
+
+//Testando o Modo depuração
+console.log('Vai começar...')
+for(var c= 1; c <=5; c++) {
+    console.log(c)
+}
+console.log('Fim!')

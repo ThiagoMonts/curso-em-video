bc8484f57842808423653e2bfc0f6c62d39f0435
ThiagoMonts <thiagohonorato@hotmail.com>
2022-09-17 15:07:12 -0300

    JavaScript - Aula 16

---
+++ javascript/exercicios/aula16/funcao01.js
@@ -0,0 +1,12 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+let res = parimpar(4) //Colocando o resultando dentro de uma variável
+console.log(res)
+
+//console.log(parimpar(223))

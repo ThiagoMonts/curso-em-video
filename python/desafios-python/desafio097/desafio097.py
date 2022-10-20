0606f5730828b55e67df495fed12a306f033427c
ThiagoMonts <thiagohonorato@hotmail.com>
2022-10-20 09:49:30 -0300

    Desafio 097

---
+++ python/desafios-python/desafio097/desafio097.py
@@ -0,0 +1,10 @@
+def escreva(txt):
+    tam = len(txt) + 4
+    print('~' * tam)
+    print(f'  {txt}')
+    print('~' * tam)
+
+
+escreva("Olá, Mundo!")
+escreva('Curso de Python no Youtube')
+escreva('Cev')
